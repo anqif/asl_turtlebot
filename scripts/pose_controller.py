276d64598c5b93f53b860323393ff1c63c01986d
blandry <ben.acc.landry@gmail.com>
2018-03-09 20:52:43 -0800

    basic navigation files for project

--- scripts/pose_controller.py
+++ scripts/pose_controller.py
@@ -14,21 +14,17 @@ K1 = 0.4
 K2 = 0.8
 K3 = 0.8
 
-# tells the robot to stay still
-# if it doesn't get messages within that time period
-TIMEOUT = .5
-
 # maximum velocity
-V_MAX = 0.2
+V_MAX = 0.25
 
 # maximim angular velocity
-W_MAX = 1
+W_MAX = 2
 
 class PoseController:
 
     def __init__(self):
         rospy.init_node('turtlebot_pose_controller', anonymous=True)
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # current state
         self.x = 0.0
@@ -40,75 +36,52 @@ class PoseController:
         self.y_g = 0.0
         self.theta_g = 0.0
 
-        # time last pose command was received
-        self.cmd_pose_time = rospy.get_rostime()
+        self.trans_listener = tf.TransformListener()
 
-        rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
-
-        ### YOUR CODE HERE ###
-
-        # create a subscriber that receives Pose2D messages and
-        # calls cmd_pose_callback. It should subscribe to '/cmd_pose'
-
-        ### END OF YOUR CODE ###
-
-    def gazebo_callback(self, data):
-        if "turtlebot3_burger" in data.name:
-            pose = data.pose[data.name.index("turtlebot3_burger")]
-            twist = data.twist[data.name.index("turtlebot3_burger")]
-            self.x = pose.position.x
-            self.y = pose.position.y
-            quaternion = (
-                pose.orientation.x,
-                pose.orientation.y,
-                pose.orientation.z,
-                pose.orientation.w)
-            euler = tf.transformations.euler_from_quaternion(quaternion)
-            self.theta = euler[2]
+        rospy.Subscriber('/cmd_pose', Pose2D, self.cmd_pose_callback)
 
     def cmd_pose_callback(self, data):
         self.x_g = data.x
         self.y_g = data.y
         self.theta_g = data.theta
-        self.cmd_pose_time = rospy.get_rostime()
+        self.run_pose_controller()
 
-    def get_ctrl_output(self):
+    def run_pose_controller(self):
         """ runs a simple feedback pose controller """
 
-        if (rospy.get_rostime()-self.cmd_pose_time)<rospy.Duration.from_sec(TIMEOUT):
-            rel_coords = np.array([self.x-self.x_g, self.y-self.y_g])
-            R = np.array([[np.cos(self.theta_g), np.sin(self.theta_g)], [-np.sin(self.theta_g), np.cos(self.theta_g)]])
-            rel_coords_rot = np.dot(R,rel_coords)
+        try:
+            (translation,rotation) = self.trans_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+            self.x = translation[0]
+            self.y = translation[1]
+            euler = tf.transformations.euler_from_quaternion(rotation)
+            self.theta = euler[2]
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            pass
 
-            th_rot = self.theta-self.theta_g
-            rho = linalg.norm(rel_coords)
-            ang = np.arctan2(rel_coords_rot[1],rel_coords_rot[0])+np.pi
-            angs = wrapToPi(np.array([ang-th_rot, ang]))
-            alpha = angs[0]
-            delta = angs[1]
+        rel_coords = np.array([self.x-self.x_g, self.y-self.y_g])
+        R = np.array([[np.cos(self.theta_g), np.sin(self.theta_g)], [-np.sin(self.theta_g), np.cos(self.theta_g)]])
+        rel_coords_rot = np.dot(R,rel_coords)
 
-            V = K1*rho*np.cos(alpha)
-            om = K2*alpha + K1*np.sinc(2*alpha/np.pi)*(alpha+K3*delta)
+        th_rot = self.theta-self.theta_g
+        rho = linalg.norm(rel_coords)
+        ang = np.arctan2(rel_coords_rot[1],rel_coords_rot[0])+np.pi
+        angs = wrapToPi(np.array([ang-th_rot, ang]))
+        alpha = angs[0]
+        delta = angs[1]
 
-            # Apply saturation limits
-            cmd_x_dot = np.sign(V)*min(V_MAX, np.abs(V))
-            cmd_theta_dot = np.sign(om)*min(W_MAX, np.abs(om))
-        else:
-            cmd_x_dot = 0
-            cmd_theta_dot = 0
+        V = K1*rho*np.cos(alpha)
+        om = K2*alpha + K1*np.sinc(2*alpha/np.pi)*(alpha+K3*delta)
 
-        cmd = Twist()
-        cmd.linear.x = cmd_x_dot
-        cmd.angular.z = cmd_theta_dot
-        return cmd
+        # Apply saturation limits
+        cmd_x_dot = np.sign(V)*min(V_MAX, np.abs(V))
+        cmd_theta_dot = np.sign(om)*min(W_MAX, np.abs(om))
 
-    def run(self):
-        rate = rospy.Rate(10) # 10 Hz
-        while not rospy.is_shutdown():
-            ctrl_output = self.get_ctrl_output()
-            self.pub.publish(ctrl_output)
-            rate.sleep()
+        cmd_msg = Twist()
+        cmd_msg.linear.x = cmd_x_dot
+        cmd_msg.angular.z = cmd_theta_dot
+        self.cmd_vel_publisher.publish(cmd_msg)
+
 
 if __name__ == '__main__':
     pctrl = PoseController()
-    pctrl.run()
+    rospy.spin()
--- scripts/supervisor.py
+++ scripts/supervisor.py
@@ -3,20 +3,15 @@
 import rospy
 from gazebo_msgs.msg import ModelStates
 from std_msgs.msg import Float32MultiArray, String
-from geometry_msgs.msg import Twist, PoseArray, Pose2D
+from geometry_msgs.msg import Twist, PoseArray, Pose2D, PoseStamped
 from asl_turtlebot.msg import DetectedObject
 import tf
 import math
 from enum import Enum
 
-# hardcoded pose goal
-x_g = 1.5
-y_g = -4.0
-theta_g = 0
-
 # threshold at which we consider the robot at a location
 POS_EPS = .1
-THETA_EPS = .15
+THETA_EPS = .3
 
 # time to stop at a stop sign
 STOP_TIME = 3
@@ -47,59 +42,112 @@ class Supervisor:
         self.y = 0
         self.theta = 0
 
+        # pose goal
+        self.x_g = 0
+        self.y_g = 0
+        self.theta_g = 0
+
         # current mode
-        self.mode = Mode.POSE
+        self.mode = Mode.IDLE
         self.last_mode_printed = None
 
+        self.nav_goal_publisher = rospy.Publisher('/cmd_nav', Pose2D, queue_size=10)
         self.pose_goal_publisher = rospy.Publisher('/cmd_pose', Pose2D, queue_size=10)
+        self.cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
         rospy.Subscriber('/detector/stop_sign', DetectedObject, self.stop_sign_detected_callback)
-        rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
+        rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.rviz_goal_callback)
 
-    def gazebo_callback(self, msg):
-        """ callback for state of the turtlebot from Gazebo """
+        self.trans_listener = tf.TransformListener()
 
-        pose = msg.pose[msg.name.index("turtlebot3_burger")]
-        twist = msg.twist[msg.name.index("turtlebot3_burger")]
-        self.x = pose.position.x
-        self.y = pose.position.y
-        quaternion = (
-            pose.orientation.x,
-            pose.orientation.y,
-            pose.orientation.z,
-            pose.orientation.w)
-        euler = tf.transformations.euler_from_quaternion(quaternion)
-        self.theta = euler[2]
+    def rviz_goal_callback(self, msg):
+        """ callback for a pose goal sent through rviz """
+
+        self.x_g = msg.pose.position.x
+        self.y_g = msg.pose.position.y
+        rotation = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
+        euler = tf.transformations.euler_from_quaternion(rotation)
+        self.theta_g = euler[2]
+
+        self.mode = Mode.NAV
 
     def stop_sign_detected_callback(self, msg):
         """ callback for when the detector has found a stop sign. Note that
-        a distance of 0 can mean that the lidar did not pickup the stop sign at all
-        so you shouldn't necessarily stop then """
+        a distance of 0 can mean that the lidar did not pickup the stop sign at all """
 
-        ### YOUR CODE HERE ###
+        # distance of the stop sign
+        dist = msg.distance
 
-        pass
+        # if close enough and in nav mode, stop
+        if dist > 0 and dist < STOP_MIN_DIST and self.mode == Mode.NAV:
+            self.init_stop_sign()
 
-        ### END OF YOUR CODE ###
+    def go_to_pose(self):
+        """ sends the current desired pose to the pose controller """
+
+        pose_g_msg = Pose2D()
+        pose_g_msg.x = self.x_g
+        pose_g_msg.y = self.y_g
+        pose_g_msg.theta = self.theta_g
+
+        self.pose_goal_publisher.publish(pose_g_msg)
+
+    def nav_to_pose(self):
+        """ sends the current desired pose to the naviagtor """
+
+        nav_g_msg = Pose2D()
+        nav_g_msg.x = self.x_g
+        nav_g_msg.y = self.y_g
+        nav_g_msg.theta = self.theta_g
+
+        self.nav_goal_publisher.publish(nav_g_msg)
+
+    def stay_idle(self):
+        """ sends zero velocity to stay put """
+
+        vel_g_msg = Twist()
+        self.cmd_vel_publisher.publish(vel_g_msg)
 
     def close_to(self,x,y,theta):
         """ checks if the robot is at a pose within some threshold """
 
         return (abs(x-self.x)<POS_EPS and abs(y-self.y)<POS_EPS and abs(theta-self.theta)<THETA_EPS)
 
-    def go_to_pose(self):
-        """ sends the current desired pose to the pose controller """
+    def init_stop_sign(self):
+        """ initiates a stop sign maneuver """
 
-        pose_g_msg = Pose2D()
-        pose_g_msg.x = x_g
-        pose_g_msg.y = y_g
-        pose_g_msg.theta = theta_g
+        self.stop_sign_start = rospy.get_rostime()
+        self.mode = Mode.STOP
 
-        self.pose_goal_publisher.publish(pose_g_msg)
+    def has_stopped(self):
+        """ checks if stop sign maneuver is over """
+
+        return (self.mode == Mode.STOP and (rospy.get_rostime()-self.stop_sign_start)>rospy.Duration.from_sec(STOP_TIME))
+
+    def init_crossing(self):
+        """ initiates an intersection crossing maneuver """
+
+        self.cross_start = rospy.get_rostime()
+        self.mode = Mode.CROSS
+
+    def has_crossed(self):
+        """ checks if crossing maneuver is over """
+
+        return (self.mode == Mode.CROSS and (rospy.get_rostime()-self.cross_start)>rospy.Duration.from_sec(CROSSING_TIME))
 
     def loop(self):
         """ the main loop of the robot. At each iteration, depending on its
         mode (i.e. the finite state machine's state), if takes appropriate
         actions. This function shouldn't return anything """
+
+        try:
+            (translation,rotation) = self.trans_listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+            self.x = translation[0]
+            self.y = translation[1]
+            euler = tf.transformations.euler_from_quaternion(rotation)
+            self.theta = euler[2]
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            pass
 
         # logs the current mode
         if not(self.last_mode_printed == self.mode):
@@ -108,41 +156,42 @@ class Supervisor:
 
         # checks wich mode it is in and acts accordingly
         if self.mode == Mode.IDLE:
-            # not doing anything
-            pass
+            # send zero velocity
+            self.stay_idle()
 
         elif self.mode == Mode.POSE:
             # moving towards a desired pose
-            if self.close_to(x_g,y_g,theta_g):
+            if self.close_to(self.x_g,self.y_g,self.theta_g):
                 self.mode = Mode.IDLE
             else:
                 self.go_to_pose()
 
         elif self.mode == Mode.STOP:
             # at a stop sign
-
-            ### YOUR CODE HERE ###
-
-            pass
-
-            ### END OF YOUR CODE ###
-
+            if self.has_stopped():
+                self.init_crossing()
+            else:
+                pass
 
         elif self.mode == Mode.CROSS:
             # crossing an intersection
+            if self.has_crossed():
+                self.mode = Mode.NAV
+            else:
+                self.nav_to_pose()
 
-            ### YOUR CODE HERE ###
-
-            pass
-
-            ### END OF YOUR CODE ###
+        elif self.mode == Mode.NAV:
+            if self.close_to(self.x_g,self.y_g,self.theta_g):
+                self.mode = Mode.IDLE
+            else:
+                self.nav_to_pose()
 
         else:
             raise Exception('This mode is not supported: %s'
                 % str(self.mode))
 
     def run(self):
-        rate = rospy.Rate(5) # 5 Hz
+        rate = rospy.Rate(10) # 10 Hz
         while not rospy.is_shutdown():
             self.loop()
             rate.sleep()
